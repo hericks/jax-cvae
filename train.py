37b20877b0b941283c7c4ef91af8d926dcb3a6f7
Matthias Hericks <matthias@robots.ox.ac.uk>
2023-08-13 00:07:06 +0200

    Extend mnist() function to load train labels.

--- train.py
+++ train.py
@@ -10,21 +10,39 @@ SEED = 42
 BATCH_SIZE = 5123
 
 def mnist():
-    filename = "train-images-idx3-ubyte.gz"
     url_dir = "https://storage.googleapis.com/cvdf-datasets/mnist"
     target_dir = os.getcwd() + "/data/mnist"
-    url = f"{url_dir}/{filename}"
-    target = f"{target_dir}/{filename}"
+    filenames = {
+        "images": "train-images-idx3-ubyte.gz",
+        "labels": "train-labels-idx1-ubyte.gz",
+    }
 
-    if not os.path.exists(target):
-        os.makedirs(target_dir, exist_ok=True)
-        urllib.request.urlretrieve(url, target)
-        print(f"Downloaded {url} to {target}")
+    # download images and labels into data folder
+    for _, filename in filenames.items():
+        url = f"{url_dir}/{filename}"
+        target = f"{target_dir}/{filename}"
+        if not os.path.exists(target):
+            os.makedirs(target_dir, exist_ok=True)
+            urllib.request.urlretrieve(url, target)
+            print(f"Downloaded {url} to {target}")
 
+    # load images into memory
+    target = f"{target_dir}/{filenames['images']}"
     with gzip.open(target, "rb") as fh:
         _, batch, rows, cols = struct.unpack(">IIII", fh.read(16))
         shape = (batch, 1, rows, cols)
-        return jnp.frombuffer(fh.read(), dtype=jnp.uint8).reshape(shape)
+        images = jnp.frombuffer(fh.read(), dtype=jnp.uint8).reshape(shape)
+
+    # load labels into memory 
+    target = f"{target_dir}/{filenames['labels']}"
+    with gzip.open(target, "rb") as fh:
+        _, batch = struct.unpack(">II", fh.read(8))
+        shape = (batch, 1,)
+        labels = jnp.frombuffer(fh.read(), dtype=jnp.uint8).reshape(shape)
+    
+    return images, labels
+
+
 
 def infinite_dataloader(data, batch_size, *, rng):
     dataset_size = data.shape[0]
@@ -42,7 +60,7 @@ def infinite_dataloader(data, batch_size, *, rng):
 
 if __name__ == "__main__":
 
-    data = mnist()
+    data, labels = mnist()
     dataset_size = data.shape[0]
     n_batches_per_epoch = (dataset_size + BATCH_SIZE - 1) // BATCH_SIZE
 
